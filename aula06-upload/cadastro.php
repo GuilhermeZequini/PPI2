6004aa3db5dfb3b2f0ace5eef6da07dcc717cc19
Guilherme Zequini <guilherzb02@gmail.com>
2024-10-01 16:26:45 -0300

    aula06

---
+++ aula06-upload/cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de receita </h1>
+    
+    <form action="file.php" method="post">
+    <p>
+        <input type="File" >
+    </p>
+
+    <p>
+        <input type="submit">
+    </p>
+
+    </form>
+</body>
+</html>
