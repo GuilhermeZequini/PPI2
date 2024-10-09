b1a4e388fcc9b474428c8872c7324a4dc25af2b1
Guilherme <guilherzb02@gmail.com>
2024-10-09 11:33:51 -0300

    tentando conectar ao banco

--- Loja-de-Roupa/index.php
+++ Loja-de-Roupa/index.php
@@ -7,6 +7,7 @@
     <title>Loja de roupa </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="">
 </head>
 
 <body>
@@ -37,6 +38,7 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="addCard.php">Adicionar Crad</a></li>
+                                <li><a class="dropdown-item" href="testesbd/teste_bd.php">testeBD</a></li>
                             </ul>
                         </li>
                     </ul>
--- Loja-de-Roupa/testesBd/teste_bd.php
+++ Loja-de-Roupa/testesBd/teste_bd.php
@@ -4,11 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>teste</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    
 </head>
 
 <body>
@@ -34,27 +32,29 @@
         </nav>
     </header>
 
+
     <main>
-        <form action="file.php" method="post">
-            <p>
-                <input type="File">
-            </p>
+        <?php
 
-            <p>
-                <input type="submit">
-            </p>
+        $h1 = "<h1>Relô o olho!</h1>";
+        #$h1 = 2;
+        echo $h1;
+        ?>
 
+        <h2>Cadastro de teste</h2>
+
+        <form action="cadastrarTeste.php" method="post">
+            <p>
+                <input type="text" name="descricao" placeholder="Descrição do teste...">
+            </p>
+            <p>
+                <input type="submit" value="Cadastrar">
+            </p>
         </form>
-        
     </main>
 
-    <footer></footer>
+
 
 </body>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
-
 
 </html>
